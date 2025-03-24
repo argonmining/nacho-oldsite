e89ce7c2b8fd1fee3d14f292af7dd6da4e635d55
Ashton <ashton@onargon.com>
2025-03-24 00:27:15 -0700

    gate

--- src/config/exchanges.ts
+++ src/config/exchanges.ts
@@ -9,7 +9,7 @@ const exchanges: Exchange[] = [
 	{
 		name: 'KSPR Bot',
 		description:
-			'KSPR Bot offers seamless access to the Kasplex protocol, allowing you to deploy, mint, and transfer KRC20 tokens with ease.',
+			'',
 		logo: '/img/exchanges/kspr-bot.png',
 		id: 'kspr-bot',
 		url: 'https://t.me/kspr_home_bot?start=nacho'
@@ -17,7 +17,7 @@ const exchanges: Exchange[] = [
 	{
 		name: 'Pionex',
 		description:
-			'Pionex, established in 2019, is a crypto exchange known for its innovative trading bots and minimal fees of 0.05%. Based in the British Virgin Islands.',
+			'',
 		logo: '/img/exchanges/pionex.svg',
 		id: 'pionex',
 		url: 'https://pionex.com/en/signUp?r=GONACHO'
@@ -25,7 +25,7 @@ const exchanges: Exchange[] = [
 	{
 		name: 'PionexUS',
 		description:
-			"PionexUS brings the power of Pionex's automated trading to the US market, offering advanced trading bots and low fees of 0.05%, tailored for US-based users.",
+			'',
 		logo: '/img/exchanges/pionex.svg',
 		id: 'pionex-us',
 		url: 'https://accounts.pionex.us/en/signup?ref=NACHO'
@@ -33,23 +33,23 @@ const exchanges: Exchange[] = [
 	{
 		name: 'CoinEx',
 		description:
-			'CoinEx, a centralized exchange launched in 2017, supports over 1,200 cryptocurrencies with competitive trading fees of 0.1%. Registered in Samoa.',
+			'',
 		logo: '/img/exchanges/coinex.svg',
 		id: 'coinex',
 		url: 'https://www.coinex.com/register?refer_code=6zrua'
 	},
 	{
-		name: 'Coinstore',
+		name: 'Gate.io',
 		description:
-			'Coinstore is a world-leading crypto trading platform that focuses on emerging high-growth markets. Trusted by more than 8.1 million users in 175 countries',
-		logo: '/img/exchanges/coinstore.png',
-		id: 'coinstore',
-		url: 'https://coinstore.com/signup?invitCode=hrntWC75'
+			'',
+		logo: '/img/exchanges/gate.svg',
+		id: 'gate',
+		url: 'https://www.gate.io/signup/VLUWXFEMAQ?ref_type=102'
 	},
 	{
 		name: 'AscendEX',
 		description:
-			'AscendEX is a global cryptocurrency platform, with a comprehensive product suite, including spot, margin, and futures trading for 200+ blockchain projects.',
+			'',
 		logo: '/img/exchanges/ascendex.png',
 		id: 'ascendex',
 		url: 'https://ascendex.com/en/register?inviteCode=UQ0BAVVXS'
